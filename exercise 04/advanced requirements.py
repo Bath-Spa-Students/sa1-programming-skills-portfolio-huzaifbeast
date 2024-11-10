b9c8fad3a2dd96d5bc1b8a9b35727159f9f68c3a
huzaifbeast <muhammedhuzaifaawan@gmail.com>
2024-11-10 07:51:33 +0400

    exercise 4 pr quiz .py

---
+++ exercise 04/advanced requirements.py
@@ -0,0 +1,85 @@
+"""
+### Advanced Requirements:
+Ignore Capitalization: Modify your program to accept answers regardless of the capitalization (e.g., "paris", "Paris", and "PaRis" should all be considered correct).
+Multiple Questions: Extend the program into a quiz that asks for the capitals of 10 European countries. Provide feedback for each question.
+
+"""
+
+#answer 
+
+country=input("what is the capital of Germany?") # 1/10
+
+if country.lower()=="berlin":
+    print("answer is correct")
+else:
+    print("answer is wrong")    
+
+    country=input("what is capital of England?") # 2/10 
+
+    if country.lower()=="london":
+        print("answer is correct")
+    else:
+        print("answer is wrong")
+
+
+country=input("what is capital of Scotland?") # 3/10
+
+if country.lower()=="edinburgh":
+    print("answer is correct")
+else:
+    print("answe is wrong ") 
+
+    country=input("what is capital of Albania ?") # 4/10
+
+    if country.lower()=="tirana":
+        print("answer is correct")
+    else:
+        print("answer is wrong ")
+
+
+country=input("what is capital of Belgium?") # 5/10
+
+if country.lower()=="brussels":
+    print("answer is correct")
+else:
+    print("answer is wrong")
+
+
+country=input("what is capital Crotia?") # 6/10
+
+if country.lower()=="zagreb":
+    print("answer is correct")
+else:
+    print("answer is wrong")
+
+
+country=input("what is capital of Bulgaria?") # 7/10
+
+if country.lower()=="sofia":
+    print("answer is correct")
+else:
+    print('answer is wrong')
+
+
+country =input("what is capital of Finland?") # 8/10
+
+if country.lower()=="helsinki":
+    
+    print("answer is correct")
+else:
+    print("answer is wrong")
+
+country=input("what is capital of Georgia") # 9/10
+
+if country.lower()=="tbilisi":
+    print("answer is correct")
+else:
+    print("answer is wrong ")
+
+country=input("what is capital of Portugal ?")  # 10/10      
+
+if country.lower()=="lisbon":
+    print("answer is correct")
+else:
+    print("answer is wrong")    
+
---
+++ exercise 04/premative quiz.py
@@ -0,0 +1,15 @@
+"""
+## Steps:
+1. Write a program that asks the user "What is the capital of France?" and waits for their response.
+2. If the user's answer is correct (i.e., "Paris"), the program should print a message saying the answer is correct.
+3. If the answer is incorrect, the program should print a message saying the answer is wrong.
+
+"""
+
+#answer
+country=input("what is the capital of France? ") #asking question 
+
+if country =="Paris": #the answer to the question 
+    print("answer is correct")
+else:
+    print("answer is wrong")
