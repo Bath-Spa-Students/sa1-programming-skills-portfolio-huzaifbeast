e95bf2ca91f37a490a35ba40489b6eabca51940f
huzaifbeast <muhammedhuzaifaawan@gmail.com>
2024-11-19 08:08:45 +0400

    Hello.py

---
+++ exercise 09/Hello.py
@@ -0,0 +1,27 @@
+"""
+Fill in the blanks in the code below so that the function hello() prints "Hello" to the console.
+```python
+
+def hello():
+    ____  # Fill in this blank to print "Hello" to the console
+
+def main():
+    ____  # Fill in this blank to call the hello() function
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+
+#answer
+
+def hello():
+    print("Hello")   # to print Hello to the console 
+
+def main():
+    hello()
+
+if __name__ == "__main__":
+    main()
+
