99a69014a77c18d025daa3de886c2b38453a79cd
huzaifbeast <muhammedhuzaifaawan@gmail.com>
2024-11-21 10:46:55 +0400

    bonus exercise lists.py

---
+++ exercise 08/bonus exercise lists.py
@@ -0,0 +1,12 @@
+"""
+Store the names of a few of your friends in a list called names. Print each person’s name
+by accessing each element in the list, one at a time.
+
+"""
+#Answer
+freinds = ["Hamza", "Sarim", "Abdullah", "Usman"]
+
+print(freinds[0])  #  to print First name
+print(freinds[1])  # to print Second name
+print(freinds[2])  # to print Third name
+print(freinds[3])  # to print Fourth name
