57431e07bd92adb85af2f5617a87ac548fc5acd1
huzaifbeast <muhammedhuzaifaawan@gmail.com>
2024-11-22 08:57:42 +0400

    bonus exercise dictt .py

---
+++ exercise 05/bonus exercise dictt .py
@@ -0,0 +1,27 @@
+"""
+Use a dictionary to store information about a person you know.Store their first name,
+last name, age, and the city in which they live.
+
+You should have keys such as first_name, last_name, age, and city. Print each piece of
+information stored in your dictionary.
+
+
+"""
+
+
+#ANSWER
+
+
+# making a dictionary
+freind = {
+    "first_name": "Hassan",
+    "last_name": "Satti",
+    "age": 17,
+    "city": "Sharjah"
+}
+
+# Printing each piece of information
+print("First Name:", freind["first_name"])
+print("Last Name:", freind["last_name"])
+print("Age:", freind["age"])
+print("City:", freind["city"])
