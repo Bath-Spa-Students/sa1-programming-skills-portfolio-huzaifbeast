f9b4168c63113fcb045fa1eef227f68bd8899321
huzaifbeast <muhammedhuzaifaawan@gmail.com>
2024-11-22 08:30:47 +0400

    bonusexercise varibles.py

---
+++ exercise 02/bonusexercise varibles.py
@@ -0,0 +1,13 @@
+"""
+Assign a message to a variable, and print that message. Then change the value of the
+variable to a new message, and print the new message.
+
+"""
+
+#ANSWER
+
+message="Messi is the best footballer in history of football." # first varible
+print(message)
+
+message="Ronaldo is also  best player in the history of football."  #second varible
+print(message)
