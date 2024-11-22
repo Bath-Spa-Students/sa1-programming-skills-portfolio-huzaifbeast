be5bdb10a8d4abf9e129c2eeeebbea3d83590c94
huzaifbeast <muhammedhuzaifaawan@gmail.com>
2024-11-22 07:41:07 +0400

    bonusexercise.py

---
+++ exercise 02/bonusexercise.py
@@ -0,0 +1,13 @@
+"""
+Use a variable to represent your favorite number. Then, using that variable, create a
+message that reveals your favorite number. Print that message.
+
+
+"""
+
+#ANSWER
+
+fav_num=26
+
+message=f"My fav number is {fav_num}" 
+print(message)
