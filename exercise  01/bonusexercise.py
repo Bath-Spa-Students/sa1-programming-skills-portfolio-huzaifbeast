d490644175674b26015b9b864f7c058a35f5b945
huzaifbeast <muhammedhuzaifaawan@gmail.com>
2024-11-22 07:32:12 +0400

    bonusexercise.py

---
+++ exercise  01/bonusexercise.py
@@ -0,0 +1,14 @@
+"""
+Write three strings in different variables and print the output as one string.
+
+"""
+
+#answer
+
+name1="Muhammad"
+name2="Huzaifa"
+name3="Awan"
+
+#to print ib one line
+full_name= name1+ " " + name2 +" "+ name3
+print(full_name)
